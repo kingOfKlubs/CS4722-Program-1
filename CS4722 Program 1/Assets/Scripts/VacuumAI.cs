486dd5b7c00e8c3aad92469bb208d3e86347b6aa
kingOfKlubs <CalebMauldin1@outlook.com>
2018-09-07 23:33:27 -0400

    added script for vacuum

---
+++ CS4722 Program 1/Assets/Scripts/VacuumAI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VacuumAI : MonoBehaviour {
+
+    public int[,] grid = new int[3, 3];
+    public int currentPos = 0;
+
+    private bool trash = false;
+    private bool clean = true;
+
+	// Use this for initialization
+	void Start () {
+        currentPos = grid[0, 2];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    public int[,] CheckNieghbors(int[,] position)
+    {
+
+        return position;
+    }
+
+    public void CheckUp()
+    {
+        if(trash)
+        {
+
+        }
+    }
+
+}
