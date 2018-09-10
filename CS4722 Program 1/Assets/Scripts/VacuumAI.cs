4df202726015a219e3cb92f805ff5402208e05d0
Caleb Mauldin <caleb.m47@aol.com>
2018-09-10 15:55:24 -0400

    created a map render script

--- CS4722 Program 1/Assets/Scripts/VacuumAI.cs
+++ CS4722 Program 1/Assets/Scripts/VacuumAI.cs
@@ -2,32 +2,101 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VacuumAI : MonoBehaviour {
+//public class VacuumAI : MonoBehaviour
+//{
 
-    int[,] rawNodes = new int[,]
-{
-    { 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0 },
-    { 0, 0, 0, 0, 0, 0 }
-};
 
-    private void Start()
-    {
-        int rowLength = rawNodes.GetLength(0);
-        int colLength = rawNodes.GetLength(1);
-        string arrayString = "";
-        for (int i = 0; i < rowLength; i++)
-        {
-            for (int j = 0; j < colLength; j++)
-            {
-                arrayString += string.Format("{0} ", rawNodes[i, j]);
-            }
-            arrayString += System.Environment.NewLine + System.Environment.NewLine;
-        }
 
-        Debug.Log(arrayString);
-    }
+//    //grid specifics
+//    [SerializeField]
+//    private int rows;
+//    [SerializeField]
+//    private int cols;
+//    [SerializeField]
+//    private Vector2 gridSize;
+//    [SerializeField]
+//    private Vector2 gridOffset;
+//    int[,] Grid = new int[3,3];
 
-}
+//    //about cells
+//    [SerializeField]
+//    private Sprite cellSprite;
+//    private Vector2 cellSize;
+//    private Vector2 cellScale;
+//    private Vector2 cellposition;
+
+//    //vacuum
+//    [SerializeField]
+//    private Sprite vacuumSprite;
+//    public Vector2 vacuumPos;
+
+
+
+//    void Start()
+//    {
+//        Grid = new int[rows, cols];
+//        InitCells(); //Initialize all cells
+//        //SpawnVacuum(vacuumPos);
+//    }
+
+//    void InitCells()
+//    {
+//        GameObject cellObject = new GameObject();
+
+//        //creates an empty object and adds a sprite renderer component -> set the sprite to cellSprite
+//        cellObject.AddComponent<SpriteRenderer>().sprite = cellSprite;
+
+//        //catch the size of the sprite
+//        cellSize = cellSprite.bounds.size;
+
+//        //get the new cell size -> adjust the size of the cells to fit the size of the grid
+//        Vector2 newCellSize = new Vector2(gridSize.x / (float)cols, gridSize.y / (float)rows);
+
+//        //Get the scales so you can scale the cells and change their size to fit the grid
+//        cellScale.x = newCellSize.x / cellSize.x;
+//        cellScale.y = newCellSize.y / cellSize.y;
+
+//        cellSize = newCellSize; //the size will be replaced by the new computed size, we just used cellSize for computing the scale
+
+//        cellObject.transform.localScale = new Vector2(cellScale.x, cellScale.y);
+
+//        //fix the cells to the grid by getting the half of the grid and cells add and minus experiment
+//        gridOffset.x = -(gridSize.x / 2) + cellSize.x / 2;
+//        gridOffset.y = -(gridSize.y / 2) + cellSize.y / 2;
+
+//        //fill the grid with cells by using Instantiate
+//        for (int row = 0; row < rows; row++)
+//        {
+//            for (int col = 0; col < cols; col++)
+//            {
+//                Grid[row, col] = Random.Range(0, 2);
+
+//                //add the cell size so that no two cells will have the same x and y position
+//                Vector2 pos = new Vector2(col * cellSize.x + gridOffset.x + transform.position.x, row * cellSize.y + gridOffset.y + transform.position.y);
+
+//                //instantiate the game object, at position pos, with rotation set to identity
+//                GameObject cO = Instantiate(cellObject, pos, Quaternion.identity) as GameObject;
+
+//                //set the parent of the cell to GRID so you can move the cells together with the grid;
+//                cO.transform.parent = transform;
+//                Debug.Log(Grid[row, col]);
+
+
+//            }
+//            Debug.Log("");
+//        }
+
+//        //destroy the object used to instantiate the cells
+//        Destroy(cellObject);
+//    }
+
+//    //so you can see the width and height of the grid on editor
+//    void OnDrawGizmos()
+//    {
+//        Gizmos.DrawWireCube(transform.position, gridSize);
+//    }
+//    void SpawnVacuum(){
+//        GameObject vacuum = Instantiate(vacuumSprite, Grid[0, 0], Quaternion.identity);
+//    }
+
+//}
---
+++ CS4722 Program 1/Assets/TileMap.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMap : MonoBehaviour
+{
+
+    public TileType[] tileTypes;
+
+    int[,] Tiles;
+    Node[,] graph;
+
+    int mapSizeX = 3;
+    int mapSizeY = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+        Tiles = new int[mapSizeX, mapSizeY];
+
+        int x, y;
+
+        for (x = 0; x < mapSizeX; x++)
+        {
+            for (y = 0; y < mapSizeX; y++)
+            {
+                Tiles[x, y] = 0;
+            }
+        }
+        GenerateMap();
+    }
+
+    class Node
+    {
+        public List<Node> neighbors;
+        public int x;
+        public int y;
+
+        public Node()
+        {
+            neighbors = new List<Node>();
+        }
+    }
+
+    void GeneratePathFinding()
+    {
+        graph = new Node[mapSizeX, mapSizeY];
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeX; y++)
+            {
+                graph[x, y] = new Node();
+                graph[x, y].x = x;
+                graph[x, y].y = y;
+            }
+        }
+
+                for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeX; y++)
+            {
+                if (x > 0)
+                    graph[x, y].neighbors.Add(graph[x - 1, y]);
+                if (x < mapSizeX - 1)
+                    graph[x, y].neighbors.Add(graph[x + 1, y]);
+                if (y > 0)
+                    graph[x, y].neighbors.Add(graph[x, y - 1]);
+                if (y < mapSizeY - 1)
+                    graph[x, y].neighbors.Add(graph[x, y + 1]);
+            }
+        }
+    }
+
+
+    void GenerateMap()
+    {
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeX; y++)
+            {
+                TileType tt = tileTypes[Tiles[x, y]];
+                Instantiate(tt.CubePrefab, new Vector3(x, y, 0), Quaternion.identity);
+            }
+        }
+    }
+}
