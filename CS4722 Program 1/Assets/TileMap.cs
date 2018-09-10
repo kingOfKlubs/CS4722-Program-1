405aead67cce9ae75659a0f5141910b53eb99b24
kingOfKlubs <CalebMauldin1@outlook.com>
2018-09-10 17:35:51 -0400

    added distance calculation

--- CS4722 Program 1/Assets/TileMap.cs
+++ CS4722 Program 1/Assets/TileMap.cs
@@ -1,20 +1,35 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TileMap : MonoBehaviour
 {
+    public GameObject selectedUnit;
+
+    
 
     public TileType[] tileTypes;
 
     int[,] Tiles;
     Node[,] graph;
 
-    int mapSizeX = 3;
-    int mapSizeY = 3;
+    int mapSizeX = 5;
+    int mapSizeY = 5;
 
     // Use this for initialization
     void Start()
+    {
+        selectedUnit.GetComponent<Unit>().tileX = (int)selectedUnit.transform.position.x;
+        selectedUnit.GetComponent<Unit>().tileY = (int)selectedUnit.transform.position.y;
+        selectedUnit.GetComponent<Unit>().map = this;
+
+
+        GenerateMapData();
+        GeneratePathFinding();
+        GenerateMap();
+    }
+
+    void GenerateMapData()
     {
         Tiles = new int[mapSizeX, mapSizeY];
 
@@ -27,10 +42,9 @@ public class TileMap : MonoBehaviour
                 Tiles[x, y] = 0;
             }
         }
-        GenerateMap();
     }
 
-    class Node
+    public class Node
     {
         public List<Node> neighbors;
         public int x;
@@ -39,6 +53,11 @@ public class TileMap : MonoBehaviour
         public Node()
         {
             neighbors = new List<Node>();
+        }
+
+        public float DistanceTo(Node n)
+        {
+            return Vector2.Distance(new Vector2(x, y), new Vector2(n.x, n.y));
         }
     }
 
@@ -55,7 +74,7 @@ public class TileMap : MonoBehaviour
             }
         }
 
-                for (int x = 0; x < mapSizeX; x++)
+        for (int x = 0; x < mapSizeX; x++)
         {
             for (int y = 0; y < mapSizeX; y++)
             {
@@ -79,8 +98,93 @@ public class TileMap : MonoBehaviour
             for (int y = 0; y < mapSizeX; y++)
             {
                 TileType tt = tileTypes[Tiles[x, y]];
-                Instantiate(tt.CubePrefab, new Vector3(x, y, 0), Quaternion.identity);
+                GameObject go = Instantiate(tt.CubePrefab, new Vector3(x, y, 0), Quaternion.identity);
+                ClickableTile ct = go.GetComponent<ClickableTile>();
+                ct.tileX = x;
+                ct.tileY = y;
+                ct.map = this;
             }
         }
+    }
+
+    public Vector3 TileCoordToWorldCoord(int x, int y)
+    {
+        return new Vector3(x, y, 0);
+    }
+
+    //public void MoveSelectedUnitTo(int x, int y)
+    //{
+    //    selectedUnit.GetComponent<Unit>().tileX = x;
+    //    selectedUnit.GetComponent<Unit>().tileY = y;
+    //}
+
+    public void GeneratePathTo(int x, int y)
+    {
+        selectedUnit.GetComponent<Unit>().currentPath = null;
+
+        Dictionary<Node, float> dist = new Dictionary<Node, float>();
+        Dictionary<Node, Node> prev = new Dictionary<Node, Node>();
+
+        List<Node> unvisted = new List<Node>();
+
+        Node source = graph[selectedUnit.GetComponent<Unit>().tileX,selectedUnit.GetComponent<Unit>().tileY];
+        Node target = graph[x, y];
+
+        dist[source] = 0;
+        prev[source] = null;
+
+        foreach (Node v in graph)
+        {
+            if (v != source)
+            {
+                dist[v] = Mathf.Infinity;
+                prev[v] = null;
+            }
+            unvisted.Add(v);
+        }
+
+        while (unvisted.Count > 0)
+        {
+            Node u = null;
+            foreach (Node possibleU in unvisted)
+            {
+                if (u == null || dist[possibleU] < dist[u])
+                    u = possibleU;
+            }
+            if (u == target)
+            {
+                break;
+            }
+            unvisted.Remove(u);
+
+            foreach(Node v in u.neighbors)
+            {
+                float alt = dist[u] + u.DistanceTo(v);
+                if(alt < dist[v])
+                {
+                    dist[v] = alt;
+                    prev[v] = u;
+                }
+            }
+        }
+
+        if (prev[target] == null)
+        {
+            return;
+        }
+
+        List<Node> currentPath = new List<Node>();
+
+        Node curr = target;
+
+        while (curr != null)
+        {
+            currentPath.Add(curr);
+            curr = prev[curr];
+        }
+
+        currentPath.Reverse();
+
+        selectedUnit.GetComponent<Unit>().currentPath = currentPath;
     }
 }
